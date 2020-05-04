a503be5a86fd1e698d29e24d28da4ebd085c8795
akamhy <64683866+akamhy@users.noreply.github.com>
2020-05-04 16:21:24 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'waybackpy',
+  packages = ['waybackpy'],
+  version = 'v1.1',
+  license='MIT',
+  description = 'A python wrapper for Internet Archives Wayback Machine',
+  author = 'akamhy',
+  author_email = 'akash3pro@gmail.com',
+  url = 'https://github.com/akamhy/waybackpy',
+  download_url = 'https://github.com/akamhy/waybackpy/archive/v1.1.tar.gz',
+  keywords = ['wayback', 'archive', 'archive website', 'wayback machine', 'Internet Archive'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',  
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
