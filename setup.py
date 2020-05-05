d3bd5b05b5f0112c1b5abb163538ba0ec6a62646
akamhy <64683866+akamhy@users.noreply.github.com>
2020-05-05 10:50:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
-import setuptools
+import os.path
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-  long_description = fh.read()
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    long_description = f.read()
 
-setuptools.setup(
-  name = 'waybackpy',
-  packages = ['waybackpy'],
-  version = 'v1.2',
-  long_description_content_type='text/markdown',
-  long_description=long_description,
-  license='MIT',
-  description = 'A python wrapper for Internet Archives Wayback Machine',
-  author = 'akamhy',
-  author_email = 'akash3pro@gmail.com',
-  url = 'https://github.com/akamhy/waybackpy',
-  download_url = 'https://github.com/akamhy/waybackpy/archive/v1.2.tar.gz',
-  keywords = ['wayback', 'archive', 'archive website', 'wayback machine', 'Internet Archive'],
-  install_requires=[],
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',  
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',      
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+setup(
+    name = 'waybackpy',
+    packages = ['waybackpy'],
+    version = 'v1.2',
+    description = 'A python wrapper for Internet Archives Wayback Machine',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author = 'akamhy',
+    author_email = 'akash3pro@gmail.com',
+    url = 'https://github.com/akamhy/waybackpy',
+    download_url = 'https://github.com/akamhy/waybackpy/archive/v1.2.tar.gz',
+    keywords = ['wayback', 'archive', 'archive website', 'wayback machine', 'Internet Archive'],
+    install_requires=[],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',  
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',      
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        ],
 )
