bdfc72d05d01fc3b108bfbfc810ca08236d3cca4
akamhy <64683866+akamhy@users.noreply.github.com>
2020-05-07 19:16:26 +0530

    Create __version__.py

---
+++ waybackpy/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "waybackpy"
+__description__ = "A python wrapper for Internet Archive's Wayback Machine API. Archive pages and retrieve archived pages easily."
+__url__ = "https://akamhy.github.io/waybackpy/"
+__version__ = "v1.5"
+__author__ = "akamhy"
+__author_email__ = "akash3pro@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020 akamhy"
