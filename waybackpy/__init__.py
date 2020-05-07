d0a4e25cf5520a23e7f7754b7414abe8ef5a6339
akamhy <64683866+akamhy@users.noreply.github.com>
2020-05-07 14:53:09 +0530

    Update __init__.py

--- waybackpy/__init__.py
+++ waybackpy/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from .wrapper import save, near, oldest, newest, get, clean_url, url_check
+from .wrapper import save, near, oldest, newest, get, clean_url, url_check, total_archives
 
 __version__ = "v1.4"
 
