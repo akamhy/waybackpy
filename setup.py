c22749a6a3438fa68832cbaa8d5e41f497ae03ad
akamhy <64683866+akamhy@users.noreply.github.com>
2020-05-07 19:54:00 +0530

    update

--- setup.py
+++ setup.py
@@ -4,21 +4,25 @@ from setuptools import setup
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
     long_description = f.read()
 
+about = {}
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'waybackpy', '__version__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
+    
 setup(
-    name = 'waybackpy',
+    name = about['__title__'],
     packages = ['waybackpy'],
-    version = 'v1.4',
-    description = "A python wrapper for Internet Archive's Wayback Machine API. Archive pages and retrieve archived pages easily.",
+    version = about['__version__'],
+    description = about['__description__'],
     long_description=long_description,
     long_description_content_type='text/markdown',
-    license='MIT',
-    author = 'akamhy',
-    author_email = 'akash3pro@gmail.com',
-    url = 'https://github.com/akamhy/waybackpy',
+    license= about['__license__'],
+    author = about['__author__'],
+    author_email = about['__author_email__'],
+    url = about['__url__'],
     download_url = 'https://github.com/akamhy/waybackpy/archive/v1.4.tar.gz',
     keywords = ['wayback', 'archive', 'archive website', 'wayback machine', 'Internet Archive'],
     install_requires=[],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires= ">=2.7",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -28,13 +32,18 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',      
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
         ],
+    project_urls={
+        'Documentation': 'https://waybackpy.readthedocs.io',
+        'Source': 'https://github.com/akamhy/waybackpy',
+    },
 )
