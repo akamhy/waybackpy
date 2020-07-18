0104a6c76684b5b77a79961756bdc7ee9abcc8be
Akash <64683866+akamhy@users.noreply.github.com>
2020-07-18 15:51:45 +0530

    user agent ; more variants

--- tests/test_1.py
+++ tests/test_1.py
@@ -20,7 +20,7 @@ def test_url_check():
 def test_save():
     # Test for urls that exist and can be archived.
     url1="https://github.com/akamhy/waybackpy"
-    target = waybackpy.Url(url1, user_agent)
+    target = waybackpy.Url(url1, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36")
     archived_url1 = target.save()
     assert url1 in archived_url1
 
@@ -32,18 +32,18 @@ def test_save():
     # Test for urls not allowed to archive by robot.txt.
     with pytest.raises(Exception) as e_info:
         url3 = "http://www.archive.is/faq.html"
-        target = waybackpy.Url(url3, user_agent)
+        target = waybackpy.Url(url3, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:25.0) Gecko/20100101 Firefox/25.0")
         target.save()
 
     # Non existent urls, test
     with pytest.raises(Exception) as e_info:
         url4 = "https://githfgdhshajagjstgeths537agajaajgsagudadhuss8762346887adsiugujsdgahub.us"
-        target = waybackpy.Url(url3, user_agent)
+        target = waybackpy.Url(url3, "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27")
         target.save()
 
 def test_near():
     url = "google.com"
-    target = waybackpy.Url(url, user_agent)
+    target = waybackpy.Url(url, "Mozilla/5.0 (Windows; U; Windows NT 6.0; de-DE) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4")
     archive_near_year = target.near(year=2010)
     assert "2010" in archive_near_year
 
@@ -53,7 +53,7 @@ def test_near():
     archive_near_day_month_year = target.near(year=2006, month=11, day=15)
     assert ("20061114" in archive_near_day_month_year) or ("20061115" in archive_near_day_month_year) or ("2006116" in archive_near_day_month_year)
 
-    target = waybackpy.Url("www.python.org", user_agent)
+    target = waybackpy.Url("www.python.org", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246")
     archive_near_hour_day_month_year = target.near(year=2008, month=5, day=9, hour=15)
     assert ("2008050915" in archive_near_hour_day_month_year) or ("2008050914" in archive_near_hour_day_month_year) or ("2008050913" in archive_near_hour_day_month_year)
 
@@ -86,19 +86,19 @@ def test_total_archives():
 
 if __name__ == "__main__":
     test_clean_url()
-    print(".")
+    print(".") #1
     test_url_check()
-    print(".")
+    print(".") #1
     test_get()
-    print(".")
+    print(".") #3
     test_near()
-    print(".")
+    print(".") #4
     test_newest()
-    print(".")
+    print(".") #5
     test_save()
-    print(".")
+    print(".") #6
     test_oldest()
-    print(".")
+    print(".") #7
     test_total_archives()
-    print(".")
+    print(".") #8
     print("OK")
