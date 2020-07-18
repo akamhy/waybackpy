11059c960eaddc36a88b88f5481b133fc7b5930c
Akash <64683866+akamhy@users.noreply.github.com>
2020-07-18 19:27:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author = about['__author__'],
     author_email = about['__author_email__'],
     url = about['__url__'],
-    download_url = 'https://github.com/akamhy/waybackpy/archive/2.0.1.tar.gz',
+    download_url = 'https://github.com/akamhy/waybackpy/archive/2.0.2.tar.gz',
     keywords = ['wayback', 'archive', 'archive website', 'wayback machine', 'Internet Archive'],
     install_requires=[],
     python_requires= ">=2.7",
