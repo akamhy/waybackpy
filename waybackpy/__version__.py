3ec61758b3bed53f0a227b9b472dcfb69ef2b553
Akash <64683866+akamhy@users.noreply.github.com>
2020-07-19 23:16:13 +0530

    Update __version__.py

--- waybackpy/__version__.py
+++ waybackpy/__version__.py
@@ -3,7 +3,7 @@
 __title__ = "waybackpy"
 __description__ = "A Python library that interfaces with the Internet Archive's Wayback Machine API. Archive pages and retrieve archived pages easily."
 __url__ = "https://akamhy.github.io/waybackpy/"
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 __author__ = "akamhy"
 __author_email__ = "akash3pro@gmail.com"
 __license__ = "MIT"
