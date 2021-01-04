d98c4f32ad8b7d8e92276cd84379909a646cf0c5
Akash Mahanty <akamhy@yahoo.com>
2021-01-05 01:48:54 +0530

    v2.3.3

--- waybackpy/__version__.py
+++ waybackpy/__version__.py
@@ -1,12 +1,10 @@
-# -*- coding: utf-8 -*-
-
 __title__ = "waybackpy"
 __description__ = (
     "A Python package that interfaces with the Internet Archive's Wayback Machine API. "
     "Archive pages and retrieve archived pages easily."
 )
 __url__ = "https://akamhy.github.io/waybackpy/"
-__version__ = "2.3.2"
+__version__ = "2.3.3"
 __author__ = "akamhy"
 __author_email__ = "akamhy@yahoo.com"
 __license__ = "MIT"
