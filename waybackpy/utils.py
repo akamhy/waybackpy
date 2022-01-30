004027f73bda4cc16fe271023aaa6819b9b8fd06
Akash Mahanty <akamhy@yahoo.com>
2022-01-30 13:28:13 +0530

    waybackpy/utils.py : Add a new function(latest_version_github) to fetch the latest release from github api and renamed latest_version to latest_version_pypi as now we have two functions to get the latest release.

--- waybackpy/utils.py
+++ waybackpy/utils.py
@@ -4,9 +4,19 @@ from .__version__ import __version__
 DEFAULT_USER_AGENT = "waybackpy %s - https://github.com/akamhy/waybackpy" % __version__
 
 
-def latest_version(package_name, user_agent=DEFAULT_USER_AGENT):
+def latest_version_pypi(package_name, user_agent=DEFAULT_USER_AGENT):
     request_url = "https://pypi.org/pypi/" + package_name + "/json"
     headers = {"User-Agent": user_agent}
     response = requests.get(request_url, headers=headers)
     data = response.json()
     return data["info"]["version"]
+
+
+def latest_version_github(package_name, user_agent=DEFAULT_USER_AGENT):
+    request_url = (
+        "https://api.github.com/repos/akamhy/" + package_name + "/releases?per_page=1"
+    )
+    headers = {"User-Agent": user_agent}
+    response = requests.get(request_url, headers=headers)
+    data = response.json()
+    return data[0]["tag_name"]
