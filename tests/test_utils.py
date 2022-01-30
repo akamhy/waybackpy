b8b9bc098f55d6d77811e4213b026ca9b7ad047e
Akash Mahanty <akamhy@yahoo.com>
2022-01-30 20:02:17 +0530

    tests/test_utils.py: test latest_version_pypi and latest_version_github of waybackpy.utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,8 @@
-from waybackpy.utils import latest_version, DEFAULT_USER_AGENT
+from waybackpy.utils import (
+    latest_version_pypi,
+    latest_version_github,
+    DEFAULT_USER_AGENT,
+)
 from waybackpy.__version__ import __version__
 
 
@@ -7,3 +11,8 @@ def test_default_user_agent():
         DEFAULT_USER_AGENT
         == "waybackpy %s - https://github.com/akamhy/waybackpy" % __version__
     )
+
+
+def test_latest_version():
+    package_name = "waybackpy"
+    assert latest_version_github(package_name) == latest_version_pypi(package_name)
