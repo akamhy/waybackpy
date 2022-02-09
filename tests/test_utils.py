87fb5ecd58a6bf22bc32e1a0a9697176ff9f5d65
Akash Mahanty <akamhy@yahoo.com>
2022-02-09 16:12:30 +0530

    remove latest version funcs from utils, they were unused.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,7 @@
+import pytest
+
 from waybackpy import __version__
-from waybackpy.utils import (
-    DEFAULT_USER_AGENT,
-    latest_version_github,
-    latest_version_pypi,
-)
+from waybackpy.utils import DEFAULT_USER_AGENT
 
 
 def test_default_user_agent() -> None:
@@ -11,8 +9,3 @@ def test_default_user_agent() -> None:
         DEFAULT_USER_AGENT
         == f"waybackpy {__version__} - https://github.com/akamhy/waybackpy"
     )
-
-
-def test_latest_version() -> None:
-    package_name = "waybackpy"
-    assert latest_version_github(package_name) == latest_version_pypi(package_name)
