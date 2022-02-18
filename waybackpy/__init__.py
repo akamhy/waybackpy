11db6152201f03dd3d33af593d4428cda2c7ca7b
Akash Mahanty <akamhy@yahoo.com>
2022-02-18 21:30:38 +0530

    undo drop python3.6, see #162

--- waybackpy/__init__.py
+++ waybackpy/__init__.py
@@ -1,6 +1,6 @@
 """Module initializer and provider of static information."""
 
-__version__ = "3.0.4"
+__version__ = "3.0.5"
 
 from .availability_api import WaybackMachineAvailabilityAPI
 from .cdx_api import WaybackMachineCDXServerAPI
